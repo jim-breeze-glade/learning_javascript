0122f2ad057d2f1c11358619ea89e21d75e16d2b
jim hinkle <breezyglade@proton.me>
2025-11-03 11:06:43 -0600

    ws-profile-card is not yet functional outside of fCC.org, working on it after I get a better understanding of how React works on my system.

---
+++ react/1b-root-render/src/main.jsx
@@ -0,0 +1,9 @@
+import { createRoot } from 'react-dom/client'
+
+createRoot(document.getElementById('root')).render(
+  <p>Hi there</p>
+)
+
+createRoot(document.getElementById('jim')).render ( 
+  <h2>jimport lol</h2>
+)
---
+++ react/5-inline-styles/src/App2.jsx
@@ -0,0 +1,21 @@
+import { usestate } from 'react'; 
+import './App.css';
+
+function Button({ buttonText }) {
+  return (
+    <button
+      style={{
+        backgroundColor: "#007BFF",
+        color: "white",
+      }}
+    >
+      {"jim"}
+    </button>
+  );
+}
+
+function App() {
+  return
+    <Button />
+}
+export default App;
